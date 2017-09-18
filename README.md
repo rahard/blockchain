cc67bafa81c76af20e35dc6c3a1406faafde1ae6
Budi Rahardjo <rahard@gmail.com>
2017-09-18 09:42:47 +0700

    added scripts

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # blockchain
-Supposed to be for a tutorial on blockchain
+Supposed to be for a tutorial on blockchain.
+This is just to show how to generate chain to a certain criteria.
+Look at the "genchain.pl" (perl) script.
+
+-- budi rahardjo (@rahard)
+september 2017
---
+++ genchain.pl
@@ -0,0 +1,40 @@
+#! /usr/bin/perl
+# a blockchain demo
+# Budi Rahardjo (@rahard)
+# 2017
+# HOW TO USE
+# $ cat genesis.txt transaction1.txt | ./genchain.pl
+
+use Digest::SHA qw(sha256_hex);
+# read input (slurp it)
+# cat transactions | this script
+my $transactions = do { local $/; <STDIN> };
+print $transactions;
+
+# fine the proper chain
+# this is just a demo to give it 100 tries
+int $i; 
+for ($i=0; $i<100; $i++) {
+   # generate chain
+   $chain = int(rand(10000));
+   print "$i - chain: $chain\n";
+   $transchain = $transactions . $chain;
+   
+   # calculate hash
+   $result=sha256_hex($transchain);
+   print "hash: $result\n";
+   
+   # do something about the hash; 
+   # take the 1st 5 digits and convert them to unsigned value
+   $valuestr = substr $result, 0, 5;
+   $value = hex($valuestr);
+   print "$valuestr $value\n";
+
+   # this is the deciding factor if you can find THE CHAIN
+   if ($value < 5000) { 
+      print "THE CHAIN: $chain\n";
+      exit(); 
+   }
+}
+
+print "CAN'T FIND THE PROPER CHAIN\n";
