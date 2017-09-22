e260bc044c4b89294f7233357d55bd1d4f743aa7
Budi Rahardjo <rahard@gmail.com>
2017-09-22 14:31:32 +0700

    update presentation material

--- genchain.pl
+++ genchain.pl
@@ -25,16 +25,20 @@ for ($i=0; $i<100; $i++) {
    print "hash: $result\n";
    
    # do something about the hash; 
+   # this is just an example for demo
    # take the 1st 5 digits and convert them to unsigned value
    $valuestr = substr $result, 0, 5;
    $value = hex($valuestr);
    print "$valuestr $value\n";
 
    # this is the deciding factor if you can find THE CHAIN
+   # if the value is less than 5000, then you found the chain
    if ($value < 5000) { 
       print "THE CHAIN: $chain\n";
       exit(); 
    }
 }
 
+# if after 100 tries chain is not found, exit
+# and perhaps run it again
 print "CAN'T FIND THE PROPER CHAIN\n";
